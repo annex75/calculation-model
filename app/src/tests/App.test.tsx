90b73d7381ed001c50f74c14c0e9fefd4fa9ede6
Toivo Säwén <sawent@strusoft.local>
2019-12-09 17:47:41 +0100

    Fixed tests

--- app/src/tests/App.test.tsx
+++ app/src/tests/App.test.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from '../App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  /* 
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-  */
+test('renders app without crashing', () => {
+    const app = shallow(<App />);
 });
